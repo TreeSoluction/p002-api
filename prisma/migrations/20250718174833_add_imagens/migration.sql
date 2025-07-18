bc82571a52299ad3fe31da8c050a23721d629600
Guilherme Vianna <mataveli91@gmail.com>
2025-07-18 14:57:22 -0300

    feat: adicionando imagens,whatsapp e instagram para telas
    Transportadora
    Restaurante
    Hospedagem
    Malharias
    Quiosques

---
+++ prisma/migrations/20250718174833_add_imagens/migration.sql
@@ -0,0 +1,14 @@
+-- AlterTable
+ALTER TABLE "hospedagens" ADD COLUMN     "produtos" TEXT[];
+
+-- AlterTable
+ALTER TABLE "malharias" ADD COLUMN     "produtos" TEXT[];
+
+-- AlterTable
+ALTER TABLE "quiosques" ADD COLUMN     "produtos" TEXT[];
+
+-- AlterTable
+ALTER TABLE "restaurantes" ADD COLUMN     "produtos" TEXT[];
+
+-- AlterTable
+ALTER TABLE "transportadoras" ADD COLUMN     "produtos" TEXT[];
