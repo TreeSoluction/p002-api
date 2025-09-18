6d65a89bf6fb9d977004e7c8b3c48c00704bece2
Guilherme Vianna <mataveli91@gmail.com>
2025-09-18 10:57:53 -0300

    hot-fix: response limit payload

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
 
   app.enableCors();
 
-  app.use(express.json({ limit: '50mb' }));
-  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+  app.use(express.json({ limit: Number.MAX_SAFE_INTEGER } as any));
+  app.use(express.urlencoded({ limit: Number.MAX_SAFE_INTEGER as any, extended: true } as any));
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
