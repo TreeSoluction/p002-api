50145f59db69a0782775744835de2d3c396d5c12
Guilherme Vianna <mataveli91@gmail.com>
2025-09-18 11:19:18 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With'],
+    credentials: true,
   });
 
   app.use(express.json({ limit: Number.MAX_SAFE_INTEGER } as any));
